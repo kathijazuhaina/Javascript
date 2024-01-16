73d16cba58c90ed43134342ab064cdab528a8b06
kathijazuhaina <114407053+kathijazuhaina@users.noreply.github.com>
2024-01-16 06:37:02 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // Treat all JS code as newer version 
+
+//alert (3 + 3);  //we are using node.js, not browsers
+
+console.log( 3 +
+    3
+); //not good practice and code readability should be high
+
+let name = "Zuhaina";
+let age=22;
+let IsLogged = false;
+let graduated = null;
+let zuh
+
+//datatypes
+
+//Primitive datatypes
+
+//number -- 2 to the pow 52 (range)
+//bigint -- for larger numbers (usually not used)
+//string -- "Zuhaina"
+//boolean -- true/false
+//null -- standalone value (to specify that certain variable is empty so that it shouldn't be undefined)
+//undefined 
+//symbol -- unique
+
+//Non Primitive datatypes
+
+//object
+
+// **********
+console.log(typeof name)
+console.log(typeof age )
+console.log(typeof zuh ); // undefined
+console.log(typeof graduated); // object
